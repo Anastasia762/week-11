9bf4bd0a54b11f85b7b2b94e6837e468f9948a43
Nastya Boykova <pacanovskayan@mail.ru>
2023-04-11 14:20:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const itemOne = 3490
+const itemTwo = 9990
+const itemThree = 7790
+const itemFour = 6990
+
+const total = itemOne + itemTwo + itemThree + itemFour
+const total = document.getElementById('total");
+console.log(+total.innerHTML);
+const total = document.getElementById('total');
+
+const discont = document.getElementById('discont');
+
+function setDisable() {
+    event.target.setAttribute('disabled', true)
+    total.innerHTML = total * 20 %
+}
